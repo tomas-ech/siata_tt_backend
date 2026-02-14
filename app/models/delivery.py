bdedca19dc3da9e6f9a97569678d247c77249381
tomas96ech <techeverri96@gmail.com>
2026-02-14 11:14:45 -0500

    delete unused import

--- app/models/delivery.py
+++ app/models/delivery.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timezone
+from .base import Base
+from datetime import datetime
 from typing import Optional, TYPE_CHECKING
 from sqlalchemy import String, Numeric, ForeignKey, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from .base import Base
 
 if TYPE_CHECKING:
     from .logistic import LandLogistic, MarineLogistic
