813c4c24977fe2ad77afdbfde5b824059b9dc2b6
tomas96ech <techeverri96@gmail.com>
2026-02-14 15:56:58 -0500

    fix: user endpoint
    added get users
    changed post response model

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -3,17 +3,27 @@ from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.dto.user import UserCreate, UserResponse
 from app.models.user import User
+from typing import List
 
 router = APIRouter()
 
-@router.post("/", response_model=UserResponse)
+@router.post("/", response_model=UserCreate)
 def create_user(user_in: UserCreate, db: Session = Depends(get_db)):
-    user_exists = db.query(User).filter(User.email == user_in.email).first()
-    if user_exists:
-        raise HTTPException(status_code=400, detail="El email ya está registrado")
+    
+    email_exists = db.query(User).filter(User.email == user_in.email).first()
+    
+    identity_exists = db.query(User).filter(User.identity_number == user_in.identity_number).first()
+    
+    if email_exists or identity_exists:
+        raise HTTPException(status_code=400, detail="El usuario ya está registrado")
     
     new_user = User(**user_in.model_dump())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.get("/", response_model=List[UserResponse])
+def get_all_users(db: Session = Depends(get_db)):
+    
+    return db.query(User).all();
